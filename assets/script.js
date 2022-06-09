c157dfb090badd3439a8b54ade3b6ac4e17ea58b
LucianoNeo <tec.info.luciano@hotmail.com>
2022-06-09 16:25:15 -0300

    game over implementado

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,10 @@ const bg = document.getElementById('bg')
 const gameover = document.getElementById('gameover')
 const reiniciar = document.getElementById('reiniciar')
 let capturados = 0
+const music = new Audio('assets/mp3/musica.mp3');
+
+
+
 
 const jump =()=>{
     pikachu.classList.add('jump')
@@ -18,12 +22,14 @@ const loop = setInterval(()=>{
 let placar = document.getElementById('placar')
 const pokePosition = pokemon.offsetLeft
 const pikaPosition = window.getComputedStyle(pikachu).bottom.replace('px','')
-console.log(pikaPosition)
 if (pokePosition <= 120 && pokePosition > 0 && pikaPosition <50){
     bg.style.animation = 'none'
     pokemon.style.animation = 'none'
     pokemon.style.left = `${pokePosition}px`
     pikachu.style.animation = 'none'
+    pikachu.style.bottom = '0'
+    pikachu.src = 'assets/img/pikaend.png'
+    pikachu.style.width= '100px'
     gameover.classList.add('gameoveranimation')
     gameover.style.opacity='1'
     reiniciar.style.display='block'
@@ -40,5 +46,8 @@ if (pokePosition == -107){
 
 },15)
 
+window.onload = ()=>{
+    music.play()
+}
 
 document.addEventListener('keydown', jump)
