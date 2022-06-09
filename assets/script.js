46a86be90e77915a1997eab08f8c91d97d6009cf
LucianoNeo <tec.info.luciano@hotmail.com>
2022-06-09 15:46:18 -0300

    mecanica basica implementada

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 const pikachu = document.getElementById('pikachu')
 const pokemon = document.getElementById('pokemon')
+const bg = document.getElementById('bg')
+const gameover = document.getElementById('gameover')
+const reiniciar = document.getElementById('reiniciar')
+let capturados = 0
 
 const jump =()=>{
     pikachu.classList.add('jump')
@@ -11,18 +15,30 @@ const jump =()=>{
 
 const loop = setInterval(()=>{
 
+let placar = document.getElementById('placar')
 const pokePosition = pokemon.offsetLeft
-
-if(pokePosition <= 50){
-    pokemon.src='assets/img/pokeball.gif'
-    pokemon.style= 'width:50px;'
-}else if (pokePosition <= -100){
-    pokemon.src=`assets/img/pokemon/pokemon (${Math.floor(Math.random() * 152)}).gif`
-    console.log(pokemon.src)
+const pikaPosition = window.getComputedStyle(pikachu).bottom.replace('px','')
+console.log(pikaPosition)
+if (pokePosition <= 120 && pokePosition > 0 && pikaPosition <50){
+    bg.style.animation = 'none'
+    pokemon.style.animation = 'none'
+    pokemon.style.left = `${pokePosition}px`
+    pikachu.style.animation = 'none'
+    gameover.classList.add('gameoveranimation')
+    gameover.style.opacity='1'
+    reiniciar.style.display='block'
+    clearInterval(loop)
 }
 
 
-},10)
+if (pokePosition == -107){
+    pokemon.src=`assets/img/pokemon/pokemon (${Math.floor(Math.random() * 152)}).gif`
+    capturados = capturados + 1
+    placar.innerText = capturados;
+    
+}
+
+},15)
 
 
 document.addEventListener('keydown', jump)
