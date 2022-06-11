b62b79e20affe5f65a8eb4c8fc9f824d936a0f98
LucianoNeo <tec.info.luciano@hotmail.com>
2022-06-11 11:37:40 -0300

    teste contador

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,7 @@ const gameoverSound = new Audio('assets/mp3/gameover.mp3');
 
 function iniciaJogo(){
     iniciar = true
+    music.play()
     jumpBtn.style.display = 'block'
     iniciarbtn.style.display = 'none'
     pikainicio.style.display = 'none'
@@ -37,9 +38,12 @@ const jump =()=>{
 }
 
 const loop = setInterval(()=>{
+    
 if (iniciar){
-const pokePosition = pokemon.offsetLeft
+const pokePosition = window.getComputedStyle(pokemon).left.replace('px','')
 const pikaPosition = window.getComputedStyle(pikachu).bottom.replace('px','')
+const pokeCapturado = pokemon.getBoundingClientRect().right.toFixed(0)
+console.log(pokeCapturado)
 if (pokePosition <= 120 && pokePosition > 0 && pikaPosition < 70){
     music.pause()
     gameoverSound.play()
@@ -56,26 +60,19 @@ if (pokePosition <= 120 && pokePosition > 0 && pikaPosition < 70){
     reiniciar.style.display='block'
     clearInterval(loop)
 }
-if (pokePosition <= -110 && pokePosition > -112){
+
+if (pokeCapturado <= 140 && pokeCapturado >= 130){
     pokemon.src=`assets/img/pokemon/pokemon (${Math.floor(Math.random() * 152)}).gif`
     capturados++
     placar.innerText = capturados;
-// if(capturados >=10 && capturados < 20){
-//     pokemon.style.animation ='animation: pokemon 2.5s infinite linear;'
-// }
-// if(capturados >=20 && capturados < 40){
-//     pokemon.style.animation ='animation: pokemon 2s infinite linear;'
-// }
-// if(capturados >=40 && capturados < 60){
-//     pokemon.style.animation ='animation: pokemon 1.5s infinite linear;'
-// }
+
     
 }
 iniciar = true
 }},15)
 
 // window.onload = ()=>{
-//     music.play()
+//    
 // }
 
 document.addEventListener('keydown', jump)
